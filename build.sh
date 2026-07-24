2e377e4ae8892234e39939ccfa1a0b33ff9a4b59
Yogesh Pandey <yogeshhpandey2004@gmail.com>
2026-07-24 11:39:17 +0530

    Reset production admin password

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; U=get_user_model(); username=os.environ['DJANGO_SUPERUSER_USERNAME']; password=os.environ['DJANGO_SUPERUSER_PASSWORD']; email=os.environ.get('DJANGO_SUPERUSER_EMAIL',''); u,_=U.objects.get_or_create(username=username, defaults={'email':email}); u.email=email; u.is_active=True; u.is_staff=True; u.is_superuser=True; u.set_password(password); u.save(); print('Superuser password reset successfully')"
